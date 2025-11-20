789d9356ea8d61053381d43c6eb6763efd727dca
Z4M55 <samuel.serna119@gmail.com>
2025-11-20 11:39:25 -0500

    Update boton suerte.py

--- pages/boton suerte.py
+++ pages/boton suerte.py
@@ -8,6 +8,23 @@ import random
 # Muestra la versión de Python junto con detalles adicionales
 st.write("Versión de Python:", platform.python_version())
 
+
+with open(image_file, "rb") as f:
+        encoded = base64.b64encode(f.read()).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }}
+       </style>
+        """,
+      unsafe_allow_html=True
+    )
+
 values = 0.0
 act1="OFF"
 
