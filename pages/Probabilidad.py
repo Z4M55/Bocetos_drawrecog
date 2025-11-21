064adbf30bd12d27c6e860c19143ec6f3e221fd2
Z4M55 <samuel.serna119@gmail.com>
2025-11-21 11:05:23 -0500

    Update Probabilidad.py

--- pages/Probabilidad.py
+++ pages/Probabilidad.py
@@ -7,26 +7,7 @@ import random
 
 
 
-#FONDO 
-def set_background(image_file: str):
-    with open(image_file, "rb") as f:
-        encoded = base64.b64encode(f.read()).decode()
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/png;base64,{encoded}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-# Usa la imagen
-set_background("fondo_bosque.png")   # ← Asegúrate que esté en la misma carpeta
+#FONDO def set_background(image_file: str): with open(image_file, "rb") as f: encoded = base64.b64encode(f.read()).decode() st.markdown( f""" <style> .stApp {{ background-image: url("data:image/png;base64,{encoded}"); background-size: cover; background-position: center; background-repeat: no-repeat; }} </style> """, unsafe_allow_html=True )
 
 
 # Muestra la versión de Python junto con detalles adicionales
